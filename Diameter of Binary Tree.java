704c5065a51198b7a3832fb87140d4ebe5813909
karthikkathera <120442160+katherakarthik@users.noreply.github.com>
2025-02-02 11:41:32 +0530

    Diameter of Binary Tree.java

--- Diameter of Binary Tree.java
+++ Diameter of Binary Tree.java
@@ -6,7 +6,7 @@ class Solution {
     }
     public int height(TreeNode root){
         if(root==null){
-            return 0;
+            return 0;                                                          //The time complexity is O(N), where N is the number of nodes in the tree, as each node is visited once.
         }
         int lef=height(root.left);
         int rig=height(root.right);
