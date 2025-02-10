1b738ba277329dfb392654b9dbca1fd9ad1f4a8f
karthikkathera <120442160+katherakarthik@users.noreply.github.com>
2025-02-10 21:33:39 +0530

    Create LIS(Longest Increasing subseqence.java

---
+++ LIS(Longest Increasing subseqence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+
+        if(n==0)return 0;
+        int maxLen=1;
+        
+        int dp[]=new int[n];
+        
+        Arrays.fill(dp,1);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+              if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+
+        }
+        return maxLen;
+        
+
+
+    }
+}
